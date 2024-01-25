772f60871fa4bb1418587d27935c7b249f15ffb6
David Xu <davidxu@Davids-MacBook-Pro.local>
2024-01-25 14:15:19 -0500

    Add Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application with TypeORM and Fastify is running on: ${await app.getUrl()}`);
 }
 bootstrap();
