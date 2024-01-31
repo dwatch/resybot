26feba3907df1d2f3278dd69da710d3f8648d478
David Xu <davidxu@macbook-pro.mynetworksettings.com>
2024-01-31 00:20:25 -0500

    Touch-ups

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Request, Post, UseGuards, Put, Body } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { SignInAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { Public } from './auth/decorator/public.decorator';
-import { UserAuthDto } from './auth/dto/auth.dto';
+import { SignupDto } from './auth/dto/auth.dto';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
   @Public()
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(SignInAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
@@ -17,7 +17,7 @@ export class AppController {
 
   @Public()
   @Put('auth/signup')
-  async signup(@Body() body: UserAuthDto) {
+  async signup(@Body() body: SignupDto) {
     const user = await this.authService.signup(body.email, body.password)
     return this.authService.login(user)
   }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,4 @@
-export class UserAuthDto {
+export class SignupDto {
   email: string
   password: string
 }
