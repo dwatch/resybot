5df28bd0a53880402d7d3c0be1ce5374f9d1bece
David Xu <davidxu@davids-mbp.mynetworksettings.com>
2024-02-05 22:32:19 -0500

    Switch from fastify to express

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { FastifyAdapter, type NestFastifyApplication } from '@nestjs/platform-fastify'
 import { readFileSync } from 'fs'
 import { join } from 'path'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap () {
   const httpsOptions = {
     key: readFileSync(join(__dirname, '../secrets/private-key.pem'), 'utf8'),
     cert: readFileSync(join(__dirname, '../secrets/public-certificate.pem'), 'utf8')
   }
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true, https: httpsOptions })
-  )
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions })
+
   await app.listen(3000, '0.0.0.0')
-  console.log(`Application with TypeORM and Fastify is running on: ${await app.getUrl()}`)
+  console.log(`Application with TypeORM and Express is running on: ${await app.getUrl()}`)
 }
 bootstrap()
