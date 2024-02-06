51ac291068f0058407bf435c869cf13b5e81f070
David Xu <davidxu@davids-mbp.mynetworksettings.com>
2024-02-06 01:13:06 -0500

    Register express session in app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module'
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { NestExpressApplication } from '@nestjs/platform-express'
+import * as session from 'express-session'
+import Redis from 'ioredis'
+import RedisStore from 'connect-redis'
 
 async function bootstrap () {
   const httpsOptions = {
@@ -10,6 +13,20 @@ async function bootstrap () {
     cert: readFileSync(join(__dirname, '../secrets/public-certificate.pem'), 'utf8')
   }
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions })
+
+  const redisClient = new Redis({ host: 'localhost', port: 6379 });
+  const redisStore = new RedisStore({ client: redisClient, prefix: "resybot:" })
+  app.use(session({
+    store: redisStore,
+    secret: readFileSync(join(__dirname, '../secrets/session-secret-key'), 'utf8'),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: true, // ensure you're using HTTPS
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  }));
 
   await app.listen(3000, '0.0.0.0')
   console.log(`Application with TypeORM and Express is running on: ${await app.getUrl()}`)
