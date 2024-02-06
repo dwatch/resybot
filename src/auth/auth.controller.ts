8cf7139996d4391c7e649ed3ce1431bfd0ac26ca
David Xu <davidxu@davids-mbp.mynetworksettings.com>
2024-02-05 23:17:02 -0500

    Polish signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Request, Post, UseGuards, Put, Body } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { SignInAuthGuard } from './local-auth.guard';
 import { Public } from './decorator/public.decorator';
-import { SignupDto } from './dto/auth.dto';
+import { SignupDto } from './dto/signup.dto';
+import { CreateJwtTokenResponse } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,13 +13,13 @@ export class AuthController {
   @UseGuards(SignInAuthGuard)
   @Post('login')
   async login(@Request() req) {
-    return this.authService.login(req.user);
+    return this.authService.createJwtToken(req.user);
   }
 
   @Public()
   @Put('signup')
-  async signup(@Body() body: SignupDto) {
+  async signup(@Body() body: SignupDto): Promise<CreateJwtTokenResponse> {
     const user = await this.authService.signup(body.email, body.password)
-    return this.authService.login(user)
+    return this.authService.createJwtToken(user)
   }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,12 @@
-export class SignupDto {
-  email: string
-  password: string
-}
-
 export class LoginDto {
   email: string
   sub: string
+}
+
+export class CreateJwtTokenRequest {
+  sub: string
+}
+
+export class CreateJwtTokenResponse {
+  authToken: string
 }
